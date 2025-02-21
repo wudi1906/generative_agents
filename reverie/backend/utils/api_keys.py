ea8a9a915bb0068853f57226361a391de27630d2
wudi <wudi1906@sina.com>
2025-02-21 20:52:14 +0800

    提交

--- reverie/backend/utils/api_keys.py
+++ reverie/backend/utils/api_keys.py
@@ -4,6 +4,6 @@
 # 如果将来需要其他 API 密钥，可以在这里添加
 API_CONFIG = {
     "use_local_llm": True,
-    "llm_endpoint": "http://localhost:11434/api/generate",
+    "llm_endpoint": "http://192.168.50.137:11434",
     "default_model": "deepseek-r1:14b"
 }
